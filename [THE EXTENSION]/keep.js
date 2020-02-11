25d60a682a987c9d004e4e173251c55eb8b60db7
Animenosekai <maxence.osawa@gmail.com>
2020-02-12 00:09:27 +0100

    Starting

---
+++ [THE EXTENSION]/keep.js
@@ -0,0 +1,5 @@
+function dimension_script(){
+    height = $(window).height() + 'px';
+    width = $(window).width() + 'px';
+    width_height = String(Dimension.style.height) + " x " + String(Dimension.style.width);
+  }
